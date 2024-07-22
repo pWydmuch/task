9dd4afcaae767f8df0a37162097896be40f67715
Patryk Wydmuch <pvydmuch@gmail.com>
2024-07-22 20:06:56 +0200

    Init

---
+++ src/main/java/org/example/task/TaskApplication.java
@@ -0,0 +1,13 @@
+package org.example.task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskApplication.class, args);
+    }
+
+}
