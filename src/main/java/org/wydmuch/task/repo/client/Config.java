cfde5fefa31e1c7baaab4e86b6dc31a1ef07b91c
Patryk Wydmuch <pvydmuch@gmail.com>
2024-07-23 17:50:00 +0200

    Introduce auth token

--- src/main/java/org/wydmuch/task/repo/client/Config.java
+++ src/main/java/org/wydmuch/task/repo/client/Config.java
@@ -3,6 +3,8 @@ package org.wydmuch.task.repo.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.client.RestClient;
 import org.springframework.web.client.support.RestClientAdapter;
@@ -17,19 +19,30 @@ public class Config {
     private static final Pattern USERNAME_EXTRACTOR = Pattern.compile("/users/([^/]+)/");
 
     @Bean
-    public GithubClient githubClient(@Value("${github.api.baseurl}") String baseUrl) {
+    public GithubClient githubClient(@Value("${github.api.baseurl}") String baseUrl,
+                                     @Value("${github.api.token}") String token) {
         RestClient restClient = RestClient.builder()
-                .defaultStatusHandler(x -> x.isSameCodeAs(HttpStatus.NOT_FOUND), (req, res) -> {
-                    Matcher matcher = USERNAME_EXTRACTOR.matcher(req.getURI().toString());
-                    if (matcher.find()) {
-                        throw new UserNotFoundException(matcher.group(1));
-                    }
-                })
+                .defaultStatusHandler(x -> x.isSameCodeAs(HttpStatus.NOT_FOUND),
+                        (req, res) -> throwExceptionIfUserNotFound(req))
+                .defaultHeaders(h -> addAuthorizationHeaderIfTokenNotNull(token, h))
                 .baseUrl(baseUrl)
                 .build();
         RestClientAdapter exchangeAdapter = RestClientAdapter.create(restClient);
         HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(exchangeAdapter).build();
         return factory.createClient(GithubClient.class);
+    }
+
+    private static void throwExceptionIfUserNotFound(HttpRequest req) {
+        Matcher matcher = USERNAME_EXTRACTOR.matcher(req.getURI().toString());
+        if (matcher.find()) {
+            throw new UserNotFoundException(matcher.group(1));
+        }
+    }
+
+    private static void addAuthorizationHeaderIfTokenNotNull(String token, HttpHeaders h) {
+        if (token != null && !token.isBlank()) {
+            h.add("Authorization", "Bearer " + token);
+        }
     }
 
 }
